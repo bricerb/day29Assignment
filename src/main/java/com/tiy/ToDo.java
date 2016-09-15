0be54b143ca279e96793cfc51c57ee37bec5df98
Richard Blanch <bricerb@gmail.com>
2016-09-15 14:53:43 -0400

    Added /home page

--- src/main/java/com/tiy/ToDo.java
+++ src/main/java/com/tiy/ToDo.java
@@ -1,10 +1,42 @@
 package com.tiy;
 
+import javax.persistence.*;
+
 /**
  * Created by Brice on 9/15/16.
  */
+
+@Entity
+@Table
 public class ToDo {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
     String text;
 
-    
+    public ToDo() {
+    }
+
+    public ToDo(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
